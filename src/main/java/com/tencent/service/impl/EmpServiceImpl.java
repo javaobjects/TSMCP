4363473bb8ae2773a286e1385e4905015e2bbf83
yanbo0039 <yanbo0039@yeah.net>
2019-10-29 10:15:14 +0800

    配置org.mybatis.generator依赖GeneratorSqlmap报错仍未解决

--- src/main/java/com/tencent/service/impl/EmpServiceImpl.java
+++ src/main/java/com/tencent/service/impl/EmpServiceImpl.java
@@ -3,7 +3,7 @@ package com.tencent.service.impl;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.transaction.annotation.Transactional;
 
 import com.tencent.dao.EmpMapper;
 import com.tencent.pojo.Emp;
