52de81c88718ac385d94371990e293429661dd0a
yanbo0039 <yanbo0039@yeah.net>
2019-11-05 19:28:05 +0800

    解决本地与服务器的冲突

--- src/main/java/com/tencent/controller/TsmcpUserControl.java
+++ src/main/java/com/tencent/controller/TsmcpUserControl.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
